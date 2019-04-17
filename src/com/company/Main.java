ab73db92e5f7661a0252a98f5780228b5bcbd538
mosamray <48773739+mosamray@users.noreply.github.com>
2019-04-17 18:10:08 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        SalesLead s1 = new SalesLead(1, "Ravi", "Patel");
+        s1.printAllInformation();
+
+        SalesLead s2 = new SalesLead(2, "Mary", "Smith");
+        s2.printAllInformation();
+
+        SalesLead s3 = new SalesLead(3, "Johnny", "Jones");
+        s3.printAllInformation();
+
+    }
+}
+
+
